f25718a48c27394d78659d3aa66eaa24acdfb57c
sokolov pavel <sokolovpavel7@gmail.com>
2023-08-14 22:17:03 +0300

    rendering markup to the Navbar component

--- src/App.js
+++ src/App.js
@@ -1,51 +1,16 @@
 import './styles/main.css';
-import sun from './img/icons/sun.svg';
-import moon from './img/icons/moon.svg';
+
 import instagram from './img/icons/instagram.svg';
 import linkedIn from './img/icons/linkedIn.svg';
 import twitter from './img/icons/twitter.svg';
 import vk from './img/icons/vk.svg';
 import gitHub from './img/icons/gitHub.svg';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <nav className="nav">
-        <div className="container">
-          <div className="nav-row">
-            <a href="./index.html" className="logo">
-              <strong>Freelancer</strong> portfolio
-            </a>
-
-            <button className="dark-mode-btn">
-              <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
-              <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
-            </button>
-
-            <ul className="nav-list">
-              <li className="nav-list__item">
-                <a
-                  href="./index.html"
-                  className="nav-list__link nav-list__link--active"
-                >
-                  Home
-                </a>
-              </li>
-              <li className="nav-list__item">
-                <a href="./projects.html" className="nav-list__link">
-                  Projects
-                </a>
-              </li>
-              <li className="nav-list__item">
-                <a href="./contacts.html" className="nav-list__link">
-                  Contacts
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
+      <Navbar />
       <header className="header">
         <div className="header__wrapper">
           <h1 className="header__title">
